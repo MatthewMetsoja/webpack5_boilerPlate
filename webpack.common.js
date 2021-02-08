07ae38150ffc27fa2721f54c0b11208c789dc962
Matthew Metsoja <matthewmetsoja1@icloud.com>
2021-02-08 22:48:17 +0000

    Started project, add some dependencies

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const loader = require("sass-loader");
+
+module.exports = {
+  entry: {
+    main:"./src/index.js",
+    vendor: "./src/vendor.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        open: true,
+
+        // openPage: '/test.html' // use to open a specific page with dev server
+  },
+  plugins: [
+
+  ],
+  module: {
+    rules: [
+     
+      {
+          test: /\.html$/,
+          use: ["html-loader"]
+      },
+      {
+        test: /\.svg|png|jpg|gif$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath:"images/",
+            publicPath: 'images/',
+            esModule: false,
+          }
+        }
+      }  
+  
+    ],
+  },
+  
+
+}
---
+++ webpack.prod.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const common = require("./webpack.common");
+const {
+    merge
+} = require("webpack-merge");
+const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin"); // this one is part of node modules so no need to install.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(), // this is needed to compile the js as the css minifier overrides the default setting
+            new HtmlWebpackPlugin({
+                template: "./src/template.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            })
+
+        ],
+
+    },
+    module: {
+
+        rules: [{
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Extract css into files
+                    'css-loader', //2. Turns css into commonjs
+                    "sass-loader" //1. Turns sass into css
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new CleanWebpackPlugin(),
+    ]
+
+});

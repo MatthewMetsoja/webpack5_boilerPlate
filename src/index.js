4f6bc12ff070fcb3f31c58910cd144a8ebd866ae
Matthew Metsoja <matthewmetsoja1@icloud.com>
2021-02-11 20:18:13 +0000

    Added bootstrap icon sprite, wiped bootstrap demo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
 import "./main.scss";
 import "./test.css";
-
-
-const test = ()=>{
-    alert('hello');
-}
-
-const fryUp = {
-    bacon: 2,
-    eggs: 1,
-    toast: 2,
-    beans: 300
-}
-
-let bigFryup = {juice:3, ...fryUp}
-
-
-
-
-
-test();
-
-console.log(fryUp, bigFryup);
